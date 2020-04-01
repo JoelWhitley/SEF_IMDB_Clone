2d3638c443f98bea893bde1674e26b85e54ff763
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-04-01 20:24:55 +1100

    Created ShowDAO.java and Show.java ShowDAO.java is only a skeleton and will need fine tuning, along with the actual sql statement for String sql (line 21). Show.java also doesn't hava any functioning methods yet

---
+++ src/main/java/app/dao/ShowDAO.java
@@ -0,0 +1,50 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.dao.utils.DatabaseUtils;
+import app.model.Show;
+
+public class ShowDAO {
+	public static final String SALT = "$2a$10$h.dl5J86rGH7I8bD9bZeZe";
+
+    public static Show getUserByUsername(String username) {
+        // Fish out the results
+        List<Show> shows = new ArrayList<>();
+
+        try {
+            // Here you prepare your sql statement
+            String sql = "INSERT SQL STATEMENT HERE";
+
+            // Execute the query
+            Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            // If you have multiple results, you do a while
+            while(result.next()) {
+                shows.add(   
+                  new Show(result.getString("showTitle"))
+                  );
+            }
+
+            // Close it
+            DatabaseUtils.closeConnection(connection);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // If there is a result
+        if(!shows.isEmpty()) return shows.get(0);
+        // If we are here, something bad happened
+        return null;
+    }
+
+
+}
