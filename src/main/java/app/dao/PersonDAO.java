431d64c0f2bf589765241c7002e5fe9f6f87b972
Mitchell Slavik s3723026 <s3723026@student.rmit.edu.au>
2020-04-10 06:55:19 +1000

    added to personController amend bio in Person.java add personDAO created frame for front end person.vm

---
+++ src/main/java/app/dao/PersonDAO.java
@@ -0,0 +1,50 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.dao.utils.DatabaseUtils;
+import app.model.Person;
+
+public class PersonDAO {
+
+	public static Person getPersonByName(String fullname) {
+
+		List<Person> people = new ArrayList<>();
+
+		try {
+			// Here you prepare your sql statement
+			String sql = "SELECT * FROM person WHERE fullname = '" + fullname + "'";
+
+			// Execute the query
+			Connection connection = DatabaseUtils.connectToDatabase();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+
+			// If you have multiple results, you do a while
+        while(result.next()) {
+            // 2) Add it to the list we have prepared
+            people.add(
+              // 1) Create a new account object
+              new Person(result.getInt("person_id"), 
+            		  result.getString("fullname"), 
+            		  result.getString("role"), 
+            		  result.getDate("date"), 
+            		  result.getString("bio"))
+            );
+        }
+
+			// Close it
+			DatabaseUtils.closeConnection(connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(!people.isEmpty()) return people.get(0);
+
+		return null;
+	}
+}
