ca16bdd7c40eb677ae37cc4cd98e40485e025aae
[Rico Wu S3769980] <S3769980@student.rmit.edu.au>
2020-04-10 14:15:15 +1000

    PersonDAO search by name

--- src/main/java/app/dao/PersonDAO.java
+++ src/main/java/app/dao/PersonDAO.java
@@ -9,12 +9,59 @@ import java.util.List;
 
 import app.dao.utils.DatabaseUtils;
 import app.model.Person;
+import app.model.Show;
 
 public class PersonDAO {
 	public static final String SALT = "$2a$10$h.dl5J86rGH7I8bD9bZeZe";
 
 	
-	 
+	
+    public static Show getShowByName(String fullName) {
+        // Fish out the results
+        List<Show> shows = new ArrayList<>();
+
+        try {
+       
+
+            //sql script. Show show title by searching first name of actor
+            String sql2 = "SELECT fullname,person.role,birthdate,bio,character_name,show_title,start_year,genre,length\n" + 
+            		"FROM credits_roll,`show`,person\n" + 
+            		"WHERE credits_roll.show_id = `show`.showid "
+            		+ "AND person.person_id = credits_roll.person_id "
+            		+ "AND UPPER(fullname) LIKE '%" + fullName + "%'"
+            				+ "GROUP BY fullname;";
+           
+
+
+            // Execute the query
+            Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql2);
+
+           
+            
+            // If you have multiple results, you do a while
+            while(result.next()) {
+                shows.add(   
+                  new Show(result.getInt("showid"),result.getString("show_title"), result.getDouble("length"),
+                		  result.getBoolean("movie"),result.getBoolean("series"),result.getString("genre"),result.getInt("year"))
+                  );
+            }
+
+            // Close it
+            DatabaseUtils.closeConnection(connection);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // If there is a result
+        if(!shows.isEmpty()) return shows.get(0);
+        // If we are here, something bad happened
+        return null;
+    }
+    
     public static Person getActorByName(String fullName) {
         // Fish out the results
         List<Person> people = new ArrayList<>();
@@ -27,7 +74,8 @@ public class PersonDAO {
             		"FROM credits_roll,`show`,person\n" + 
             		"WHERE credits_roll.show_id = `show`.showid "
             		+ "AND person.person_id = credits_roll.person_id "
-            		+ "AND UPPER(fullname) LIKE '%" + fullName + "%';";
+            		+ "AND UPPER(fullname) LIKE '%" + fullName + "%'"
+            				+ "GROUP BY fullname;";
            
             //sql script. search actor by first name
 
@@ -71,7 +119,5 @@ public class PersonDAO {
         // If we are here, something bad happened
         return null;
     }
-    
-    
 
 }
