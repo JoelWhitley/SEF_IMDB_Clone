776848c00ea9a43218271d7a553a0bef406946e5
Cameron vanRoon s3382032 <s3382032@student.rmit.edu.au>
2020-04-18 20:17:25 +1000

    Created UserReview class

--- src/main/java/app/model/UserReview.java
+++ src/main/java/app/model/UserReview.java
@@ -5,7 +5,15 @@ import java.util.Date;
 
 
 
-
+/**
+ * Stores a User Review, containing UNIQUE immutable reviewID, an immutable username, an immutable date, and a rating.
+ * NB. There are unique constructors for creating a review for the first time and for reconstructing one based on database content.
+ * ALso note: Both starred ratings and written reviews use this class. Thus not all ratings will have a review.
+ * However, every written review must be accompanied by a rating in the current implementation.
+ * The review constructors call the related rating constructors to minimize code repetition.
+ * @author Cameron
+ *
+ */
 public class UserReview {
     private int reviewID;
     private String username;
@@ -15,20 +23,46 @@ public class UserReview {
 
 
 
-
-
-
-    public UserReview(String r, int v) {
-        review = r;
-        rating = v;
-        date = new Date();
+    //Used when creating a star rating for the first time
+    public UserReview(int reviewID, String username, int rating) {
+    	this.reviewID = reviewID;
+    	this.username = username;
+    	this.rating = rating;
+    	this.date = new Date();
+    }
+    
+    //Used when getting a star rating from the database.
+    public UserReview(int reviewID, String username, int rating,  Date date) {
+    	this.reviewID = reviewID;
+    	this.username = username;
+    	this.rating = rating;
+    	this.date = date;
     }
 
+    //Used when creating a written review for the first time.
+    public UserReview(int reviewID, String username, int rating, String review) {
+    	this(reviewID, username, rating);
+        this.review = review;
+    }
+    
+    //Used when getting a written review from the database.
+    public UserReview(int reviewID, String username, String review, int rating, Date date) {
+    	this(reviewID, username, rating, date);
+        this.review = review;
+    }
+    
+
+    public int getReviewID() {
+		return reviewID;
+	}
 
 
+	public String getUsername() {
+		return username;
+	}
 
 
-    public String getReview() {
+	public String getReview() {
         return review;
     }
 
