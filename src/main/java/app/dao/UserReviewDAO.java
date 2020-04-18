c96672c4c900d70c876bb2e9a655d526ce1e30dc
Cameron vanRoon s3382032 <s3382032@student.rmit.edu.au>
2020-04-18 21:00:46 +1000

    added review DAO

---
+++ src/main/java/app/dao/UserReviewDAO.java
@@ -0,0 +1,109 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import app.dao.utils.DatabaseUtils;
+import app.model.UserReview;
+
+public class UserReviewDAO {
+	
+	//Used when constructing a Review to display.
+	public static UserReview getReviewById(int id) {
+		List<UserReview> reviews =  new ArrayList<>();
+		String sql = "SELECT * FROM `user_review` WHERE reviewId ='" + id + "'";
+		
+        try {
+        	Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            // If you have multiple results, you do a while
+	        while(result.next()) {
+	            reviews.add(  
+	              new UserReview(result.getInt("reviewId"), result.getString("user_id"), result.getInt("show_id"),
+	            		  result.getInt("rating"), result.getString("review"), result.getDate("date"))
+	              );
+	        }
+	
+	        // Close it
+	        DatabaseUtils.closeConnection(connection);
+		    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	
+	
+	    // If there is a result
+	    if(!reviews.isEmpty()) return reviews.get(0);
+	    // If we are here, something bad happened
+	    return null;
+	}
+	
+	//Used when grabbing all of a show's reviews.
+	public static List<UserReview> searchReviewByShowID(int showID) {
+		List<UserReview> reviews =  new ArrayList<>();
+		String sql = "SELECT * FROM `user_review` WHERE show_id ='" + showID + "'";
+		
+        try {
+        	Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            // If you have multiple results, you do a while
+	        while(result.next()) {
+	            reviews.add(  
+	              new UserReview(result.getInt("reviewId"), result.getString("user_id"), result.getInt("show_id"),
+	            		  result.getInt("rating"), result.getString("review"), result.getDate("date"))
+	              );
+	        }
+	
+	        // Close it
+	        DatabaseUtils.closeConnection(connection);
+		    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	
+	
+	    // If there is a result
+	    if(!reviews.isEmpty()) return reviews;
+	    // If we are here, something bad happened
+	    return null;
+	}
+	
+	//Used when displaying a user's review-list.
+	public static List<UserReview> searchReviewByUsername(int username) {
+		List<UserReview> reviews =  new ArrayList<>();
+		String sql = "SELECT * FROM `user_review` WHERE user_id ='" + username + "'";
+		
+        try {
+        	Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            // If you have multiple results, you do a while
+	        while(result.next()) {
+	            reviews.add(  
+	              new UserReview(result.getInt("reviewId"), result.getString("user_id"), result.getInt("show_id"),
+	            		  result.getInt("rating"), result.getString("review"), result.getDate("date"))
+	              );
+	        }
+	
+	        // Close it
+	        DatabaseUtils.closeConnection(connection);
+		    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	
+	
+	    // If there is a result
+	    if(!reviews.isEmpty()) return reviews;
+	    // If we are here, something bad happened
+	    return null;
+	}
+
+	}
+
