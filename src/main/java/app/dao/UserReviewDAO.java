5fc6bf1bb6bbc40693ea02b868085c3eacca2903
Cameron vanRoon s3382032 <s3382032@student.rmit.edu.au>
2020-04-22 21:27:04 +1000

    formatting show page and adding some comments

--- src/main/java/app/dao/UserReviewDAO.java
+++ src/main/java/app/dao/UserReviewDAO.java
@@ -106,8 +106,10 @@ public class UserReviewDAO {
 	    return null;
 	}
 	
+	//Need to fix review-id check. Because deleted reviews will subsequently cause duplicate id numbers.
 	public static void insertReviewIntoDataBase(UserReview review) {
 		
+		//Getting number of reviews for the new reviewID
 		String countReviewsQuery = "Select COUNT(reviewId) from `user_review`;";
 		
 		try {
@@ -116,6 +118,7 @@ public class UserReviewDAO {
             ResultSet result = statement.executeQuery(countReviewsQuery);
             result.next();
             
+            //Insert query.
             String insertQuery = String.format("INSERT INTO `user_review` VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", 
             		result.getInt(1) +1, review.getShowID(), review.getUsername(), 
             		review.getRating(), review.getReview(), review.getDate());
