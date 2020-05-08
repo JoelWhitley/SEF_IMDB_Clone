6ebe1e55fad7844b27b7a6bb896911bb2205d9e7
Mitchell Slavik s3723026 <s3723026@student.rmit.edu.au>
2020-05-08 15:36:57 +1000

    updated person - updated personDAO to match database changes

--- src/main/java/app/dao/PersonDAO.java
+++ src/main/java/app/dao/PersonDAO.java
@@ -16,8 +16,7 @@ import java.util.List;
 public class PersonDAO {
 	public static final String SALT = "$2a$10$h.dl5J86rGH7I8bD9bZeZe";
 
-
-
+	
 	//returns a list of persons despite the name
     public static List<Person> getPersonByString(String personSearch) {
         // Fish out the results
@@ -73,9 +72,17 @@ public class PersonDAO {
         // If we are here, something bad happened
         return null;
     }
-    
- 
 
+    /**
+     * Method to fetch users from the database.
+     * You should use this as an example for future queries, though the sql statement
+     * will change -and you are supposed to write them.
+     *
+     * Current user: caramel 6, password (the password is "password" without quotes)
+     * @param username what the user typed in the log in form.
+     * @return Some of the user data to check on the password. Null if there
+     *         no matching user.
+     */
 	public static Person getPersonById(int id) {
 
 		List<Person> people = new ArrayList<>();
@@ -113,16 +120,6 @@ public class PersonDAO {
 		return null;
 	}
 
-	/**
-     * Method to fetch users from the database.
-     * You should use this as an example for future queries, though the sql statement
-     * will change -and you are supposed to write them.
-     *
-     * Current user: caramel 6, password (the password is "password" without quotes)
-     * @param username what the user typed in the log in form.
-     * @return Some of the user data to check on the password. Null if there
-     *         no matching user.
-     */
     public static Person getRandomActorInfo() {
         // Fish out the results
         List<Person> people = new ArrayList<>();
@@ -168,11 +165,13 @@ public class PersonDAO {
 
         try {
             // Here you prepare your sql statement
-            String sql = "SELECT `show`.*"
+            String sql = "SELECT `show`.*, production_company.proco_name"
             		+ " FROM `show`"
             		+ " JOIN credits_roll ON credits_roll.show_id=`show`.showid"
             		+ " JOIN person ON credits_roll.person_id=person.person_id"
+            		+ " JOIN production_company ON production_company.proco_id=`show`.proco_id"
             		+ " WHERE credits_roll.person_id=" + personId
+            		+ " AND `status`='VISABLE'"
             		+ " ORDER BY `year` DESC";
 
             // Execute the query
@@ -195,8 +194,8 @@ public class PersonDAO {
             					 result.getBoolean("series"),
             					 result.getString("genre"),
             					 result.getInt("year"),
-            					 showStatus.valueOf(result.getString("show_status")),
-            					 result.getString("proCo")
+            					 showStatus.valueOf(result.getString("status")),
+            					 result.getString("proco_name")
             					 )
                 );
             }
