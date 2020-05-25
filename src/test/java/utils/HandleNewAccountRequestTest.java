a496c1b1b6c1536f0c6377bf4156317b8acdea0c
[Joel Whitley s3787473] <S3787473@student.rmit.edu.au>
2020-05-25 13:18:22 +1000

    Added basic approve and deny request tests

--- src/test/java/utils/HandleNewAccountRequestTest.java
+++ src/test/java/utils/HandleNewAccountRequestTest.java
@@ -1,5 +1,6 @@
 package utils;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
@@ -15,27 +16,52 @@ import app.model.enumeration.AccountRole;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HandleNewAccountRequestTest {
-	int procoCount = 0;
-	int criticCount = 0;
+	Account testProco;
+	Account testCritic;
 	
 	@BeforeEach
 	private void setUp() {
-		List<Account> procos =  new ArrayList<>();
-		List<Account> critics =  new ArrayList<>();
+		testProco = new Account("testProco", "$2a$10$h.dl5J86rGH7I8bD9bZeZeci0pDt0.VwFTGujlnEaZXPf/q7vM5wO", 
+				"Testing", "Maipatients", "123 Fake Drive", "Testoria", "Male", "test1@call.com", AccountRole.PENDING_PROCO, "2000-01-01 00:00:00.000");
+		testCritic = new Account("testCritic", "$2a$10$h.dl5J86rGH7I8bD9bZeZeci0pDt0.VwFTGujlnEaZXPf/q7vM5wO", 
+				"Testing", "Maipatients", "124 Fake Drive", "Testoria", "Female", "test2@call.com", AccountRole.PENDING_CRITIC, "2000-01-01 00:00:00.000");
 		
-		procos = AccountDAO.getAccountsByType(AccountRole.PROCO);
-		critics = AccountDAO.getAccountsByType(AccountRole.CRITIC);
-		
-		procoCount = procos.size();
-		criticCount = critics.size();
-		System.out.print("it gets here");
+		Account testAccounts[] = {testProco,testCritic};
+		for(Account testUser:testAccounts) {
+			if(AccountDAO.getUserByUsername(testUser.getUsername())!= null) {
+				AccountDAO.deleteUserInDataBase(testUser.getUsername());
+			}
+			try{
+				AccountDAO.insertAccountIntoDataBase(testUser);
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	
 	}
 	
 	@Test
 	public void updateUserType_addsProco_ProcoRequestApproved() {
-		AccountDAO.updateUserType("jwit", AccountRole.PROCO);
-		List<Account> accounts =  new ArrayList<>();
-		accounts = AccountDAO.getAccountsByType(AccountRole.PROCO);
-		assertEquals(procoCount+1 ,accounts.size());
+		
+		AccountDAO.updateUserType(testProco.getUsername(), AccountRole.PROCO);
+		assertEquals(AccountDAO.getUserDetails(testProco.getUsername()).getType(), AccountRole.PROCO);
+	}
+	
+	@Test
+	public void updateUserType_addsCritic_CriticRequestApproved() {
+		AccountDAO.updateUserType(testCritic.getUsername(), AccountRole.CRITIC);
+		assertEquals(AccountDAO.getUserDetails(testCritic.getUsername()).getType(), AccountRole.CRITIC);
+	}
+	
+	@Test
+	public void updateUserType_revertsAccountToUser_ProcoRequestDenied() {
+		AccountDAO.updateUserType(testProco.getUsername(), AccountRole.USER);
+		assertEquals(AccountDAO.getUserDetails(testProco.getUsername()).getType(), AccountRole.USER);
+	}
+	@Test
+	public void updateUserType_revertsAccountToUser_CriticRequestDenied() {
+		AccountDAO.updateUserType(testCritic.getUsername(), AccountRole.USER);
+		assertEquals(AccountDAO.getUserDetails(testCritic.getUsername()).getType(), AccountRole.USER);
 	}
 }
